bbd3983499798a3b00d78935079506ab97dc7815
Tracy Norris <tknorris@gmail.com>
2014-06-29 23:47:20 -0400

    Readd import I shouldn't have removed.

--- service.py
+++ service.py
@@ -16,6 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import datetime
+import _strptime # fix bug in python import
 import json
 import xbmc
 import xbmcgui
